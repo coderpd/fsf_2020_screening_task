a66102092614d4b087e2d7e231bf6a86b0536604
ankur <2018352@iiitdmj.ac.in>
2020-03-09 13:11:08 +0530

    added buttons Load Validate Submit

--- application.py
+++ application.py
@@ -1,17 +1,36 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton, QApplication, QMainWindow, QTableWidgetItem, QDialog, QTabWidget, QDialogButtonBox, QTableWidget, QAction
+from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QVBoxLayout, QHBoxLayout, QPushButton, QApplication, QMainWindow, QTableWidgetItem, QDialog, QTabWidget, QDialogButtonBox, QTableWidget, QAction
 from PyQt5 import QtGui
 from PyQt5.QtGui import QIcon
 
 
-class App(QMainWindow):
+class App(QWidget):
 	def __init__(self):
 		super().__init__()
 		self.setWindowTitle("Application")
 		self.tabs = Tab(self)
-		self.setCentralWidget(self.tabs)
+		self.btns = Buttons(self)
+		self.box = QHBoxLayout(self)
+		self.box.addWidget(self.tabs)
+		self.box.addWidget(self.btns)
+		self.setLayout(self.box)
 
 		self.show()
+
+
+class Buttons(QWidget):
+	def __init__(self, parent):
+		super(Buttons, self).__init__()
+		self.load = QPushButton('Load')
+		self.validate = QPushButton('Validate')
+		self.submit = QPushButton('Submit')
+
+		self.box = QVBoxLayout(self)
+		self.box.addWidget(self.load)
+		self.box.addWidget(self.validate)
+		self.box.addWidget(self.submit)
+
+		self.setLayout(self.box)
 
 
 class Tab(QWidget):
@@ -50,7 +69,7 @@ class FinPlate(QMainWindow):
 	def __init__(self):
 		super().__init__()
 
-		self.form_widget = MyTable(20, 7)
+		self.form_widget = MyTable(10, 7)
 		self.setCentralWidget(self.form_widget)
 		col_headers = ['ID', 'Connection type', 'Axial load', 'Sher load', 'Bolt diameter', 'Bolt grade', 'Plate Thickness']
 		self.form_widget.setHorizontalHeaderLabels(col_headers)
@@ -62,7 +81,7 @@ class TensionMember(QMainWindow):
 	def __init__(self):
 		super().__init__()
 
-		self.form_widget = MyTable(20, 5)
+		self.form_widget = MyTable(10, 5)
 		self.setCentralWidget(self.form_widget)
 		col_headers = ['ID', 'Member length', 'Tensile load', 'Support condition at End 1', 'Support condition at End 2']
 		self.form_widget.setHorizontalHeaderLabels(col_headers)
@@ -74,7 +93,7 @@ class BCEndPlate(QMainWindow):
 	def __init__(self):
 		super().__init__()
 
-		self.form_widget = MyTable(20, 8)
+		self.form_widget = MyTable(10, 8)
 		self.setCentralWidget(self.form_widget)
 		col_headers = ['ID', 'End plate type', 'Sher load', 'Axial load', 'Maximum Load', 'Bolt diameter', 'Bolt grade', 'Plate thickness']
 		self.form_widget.setHorizontalHeaderLabels(col_headers)
@@ -86,7 +105,7 @@ class CheatAngle(QMainWindow):
 	def __init__(self):
 		super().__init__()
 
-		self.form_widget = MyTable(20, 7)
+		self.form_widget = MyTable(10, 7)
 		self.setCentralWidget(self.form_widget)
 		col_headers = ['ID', 'Angle leg 1', 'Angle leg 2', 'Angle thickness', 'Sher load', 'Bolt diameter', 'Bolt grade']
 		self.form_widget.setHorizontalHeaderLabels(col_headers)
